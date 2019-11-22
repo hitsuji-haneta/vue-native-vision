b7b07a0adad0433d661b5edb18c4e87ec9ca0f49
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-11-22 18:01:28 +0900

    Put labels to images on firebase storage by using Vision API.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,47 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as path from 'path';
+import * as os from 'os';
+const vision = require('@google-cloud/vision'); // to avoid noImplicitAny
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+admin.initializeApp();
+const client = new vision.ImageAnnotatorClient();
+
+export const sendToVisionAPI = functions.storage
+  .object()
+  .onFinalize(async object => {
+    const filePath = object.name;
+    if (!filePath) {
+      console.log('File path is undefined.');
+      return;
+    }
+
+    const fileName = path.basename(filePath);
+    if (!fileName) {
+      console.log('File name is undefined.');
+      return;
+    }
+
+    const contentType = object.contentType;
+    if (!contentType || !contentType.startsWith('image/')) {
+      console.log('This is not an image.');
+      return;
+    }
+
+    const fileBucket = object.bucket;
+    const bucket = admin.storage().bucket(fileBucket);
+    const tempFilePath = path.join(os.tmpdir(), fileName);
+    await bucket.file(filePath).download({ destination: tempFilePath });
+    console.log('Image downloaded locally to', tempFilePath);
+
+    client
+      .labelDetection(tempFilePath)
+      .then((results: any) => {
+        const labels = results[0].labelAnnotations;
+        console.log('Labels:');
+        labels.forEach((label: any) => console.log(label.description));
+      })
+      .catch((err: any) => {
+        console.error('ERROR:', err);
+      });
+  });
