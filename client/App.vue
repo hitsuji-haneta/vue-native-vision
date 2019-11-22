1389702a14360514a7b727479497674a3c1fd9b8
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-11-22 20:24:13 +0900

    Make src directory

--- client/App.vue
+++ client/App.vue
@@ -1,84 +1,17 @@
 <template>
   <root>
-    <view class="container">
-      <camera class="camera" :type="this.type" ref="camera" />
-      <view class="wrapper">
-        <text class="text">hello, world</text>
-        <touchable-opacity :on-press="takePicture" class="touchable">
-          <material-icons name="camera" :size="70" />
-        </touchable-opacity>
-      </view>
-    </view>
+    <camera-screen />
   </root>
 </template>
 
 <script>
-import * as Permissions from "expo-permissions";
-import { Camera } from "expo-camera";
-import { Root, Toast } from "native-base";
-import { MaterialIcons } from "@expo/vector-icons";
+import { Root } from "native-base";
+import CameraScreen from "./src/components/CameraScreen";
 
 export default {
-  data: function() {
-    return {
-      hasCameraPermission: false,
-      type: Camera.Constants.Type.back
-    };
-  },
-  mounted: async function() {
-    try {
-      const { status } = await Permissions.askAsync(Permissions.CAMERA);
-      hasCameraPermission = status === "granted";
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  methods: {
-    takePicture: async function() {
-      const pictureData = await this.$refs.camera.takePictureAsync();
-      Toast.show({
-        text: "Success",
-        buttonText: "Okay",
-        duration: 2000
-      });
-      console.log("Took a pictuer! : ", pictureData.uri);
-    }
-  },
   components: {
-    Camera,
-    MaterialIcons,
+    CameraScreen,
     Root
   }
 };
 </script>
-
-<style>
-.container {
-  background-color: white;
-  flex: 1;
-}
-.camera {
-  flex: 3;
-  background-color: transparent;
-}
-.wrapper {
-  flex: 1;
-  flex-direction: column;
-  align-items: center;
-  background-color: gainsboro;
-}
-.text {
-  flex: 1;
-  font-size: 40;
-  margin: 10;
-}
-.touchable {
-  flex: 1;
-  align-items: center;
-  align-self: center;
-}
-.button {
-  color: white;
-  background-color: blue;
-}
-</style>
---
+++ client/src/components/CameraScreen.vue
@@ -0,0 +1,81 @@
+<template>
+  <view class="container">
+    <camera class="camera" :type="this.type" ref="camera" />
+    <view class="wrapper">
+      <text class="text">hello, world!</text>
+      <touchable-opacity :on-press="takePicture" class="touchable">
+        <material-icons name="camera" :size="70" />
+      </touchable-opacity>
+    </view>
+  </view>
+</template>
+
+<script>
+import * as Permissions from "expo-permissions";
+import { Camera } from "expo-camera";
+import { Toast } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export default {
+  data: function() {
+    return {
+      hasCameraPermission: false,
+      type: Camera.Constants.Type.back
+    };
+  },
+  mounted: async function() {
+    try {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      hasCameraPermission = status === "granted";
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  methods: {
+    takePicture: async function() {
+      const pictureData = await this.$refs.camera.takePictureAsync();
+      Toast.show({
+        text: "Success",
+        buttonText: "Okay",
+        duration: 2000
+      });
+      console.log("Took a pictuer! : ", pictureData.uri);
+    }
+  },
+  components: {
+    Camera,
+    MaterialIcons
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  background-color: white;
+  flex: 1;
+}
+.camera {
+  flex: 3;
+  background-color: transparent;
+}
+.wrapper {
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  background-color: gainsboro;
+}
+.text {
+  flex: 1;
+  font-size: 40;
+  margin: 10;
+}
+.touchable {
+  flex: 1;
+  align-items: center;
+  align-self: center;
+}
+.button {
+  color: white;
+  background-color: blue;
+}
+</style>
